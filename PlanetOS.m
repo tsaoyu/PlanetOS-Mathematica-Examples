77bb6bbe364f7c1a4889c3cf96437439f7e08946
Tony Yu Cao <tsaoyu@gmail.com>
2016-10-11 13:36:12 +0100

    Set apikey as global variable

--- PlanetOS.m
+++ PlanetOS.m
@@ -10,35 +10,35 @@ Stationdata::usage = "Get vaule from specified station."
 Begin["`Private`"]
   apirooturl = "http://api.planetos.com/v1/datasets/"
   Datasets:=Module[{datasetssurl},
-	datasetssurl = apirooturl<>"?apikey="<>$apikey ;
+	datasetssurl = apirooturl<>"?apikey="<>Global`apikey ;
 	Import[datasetssurl ,"json"]
 	]
   Metadata[datasets_:"hycom_glbu0.08_91.2_global_0.08d"]:=Module[{datasetmetaurl},
-	datasetmetaurl = apirooturl<>datasets<>"?apikey="<>$apikey ;
+	datasetmetaurl = apirooturl<>datasets<>"?apikey="<>Global`apikey ;
 	Import[datasetmetaurl ,"json"]
 	]
   Pointdata[datasets_:"hycom_glbu0.08_91.2_global_0.08d",lon_,lat_,opts___?OptionQ]:=Module[{dataseturl},
 	If[opts=={},
-	dataseturl= apirooturl<>datasets<>"/point?lon="<>ToString[lon]<>"&lat="<>ToString[lat]<>"&apikey="<>$apikey;,
-	dataseturl= apirooturl<>datasets<>"/point?lon="<>ToString[lon]<>"&lat="<>ToString[lat]<>StringJoin[StringJoin[{"&",ToString[#[[1]]],"=",ToString[#[[2]]]}]&/@Flatten[opts]]<>"&apikey="<>$apikey ;
+	dataseturl= apirooturl<>datasets<>"/point?lon="<>ToString[lon]<>"&lat="<>ToString[lat]<>"&apikey="<>Global`apikey;,
+	dataseturl= apirooturl<>datasets<>"/point?lon="<>ToString[lon]<>"&lat="<>ToString[lat]<>StringJoin[StringJoin[{"&",ToString[#[[1]]],"=",ToString[#[[2]]]}]&/@Flatten[opts]]<>"&apikey="<>Global`apikey ;
 	];
 	Import[dataseturl,"json"]
 	]
    Areadata[datasets_:"hycom_glbu0.08_91.2_global_0.08d",polygon_,opts___?OptionQ]:=Module[{dataseturl},
 	If[opts=={},
-	dataseturl= apirooturl<>datasets<>"/area?polygon="<>ToString[polygon]<>"&apikey="<>$apikey;,      
-	dataseturl= apirooturl<>datasets<>"/area?polygon="<>ToString[polygon]<>StringJoin[StringJoin[{"&",ToString[#[[1]]],"=",ToString[#[[2]]]}]&/@Flatten[opts]]<>"&apikey="<>$apikey ;
+	dataseturl= apirooturl<>datasets<>"/area?polygon="<>ToString[polygon]<>"&apikey="<>Global`apikey;,      
+	dataseturl= apirooturl<>datasets<>"/area?polygon="<>ToString[polygon]<>StringJoin[StringJoin[{"&",ToString[#[[1]]],"=",ToString[#[[2]]]}]&/@Flatten[opts]]<>"&apikey="<>Global`apikey ;
 	];
 	Import[dataseturl,"json"]
 	]
    Stations[datasets_:"noaa_ndbc_stdmet_stations"]:=Module[{datasetssurl},
-	datasetssurl = apirooturl<>datasets<>"/stations?apikey="<>$apikey ;
+	datasetssurl = apirooturl<>datasets<>"/stations?apikey="<>Global`apikey ;
 	Import[datasetssurl ,"json"]
 	]
    Stationdata[datasets_:"noaa_ndbc_stdmet_stations",station_,opts___?OptionQ]:=Module[{dataseturl},
 	If[opts=={},
-	dataseturl= apirooturl<>datasets<>"/stations/"<>station<>"?apikey="<>$apikey;,
- dataseturl= apirooturl<>datasets<>"/stations/"<>station<>"?apikey="<>$apikey<>StringJoin[StringJoin[{"&",ToString[#[[1]]],"=",ToString[#[[2]]]}]&/@Flatten[opts]] ;
+	dataseturl= apirooturl<>datasets<>"/stations/"<>station<>"?apikey="<>Global`apikey;,
+ dataseturl= apirooturl<>datasets<>"/stations/"<>station<>"?apikey="<>Global`apikey<>StringJoin[StringJoin[{"&",ToString[#[[1]]],"=",ToString[#[[2]]]}]&/@Flatten[opts]] ;
 	];
 	Import[dataseturl,"json"]
 	]
